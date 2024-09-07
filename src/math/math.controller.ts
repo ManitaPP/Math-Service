5ff9898a392709e9b250fc0a801ef19cbc08b254
Manita Intharachot <64160144@go.buu.ac.th>
2024-09-07 21:06:25 +0700

    Add MessagePattern in  math controller

--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { MathService } from './math.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('math')
 export class MathController {
@@ -11,4 +12,11 @@ export class MathController {
     this.logger.log('sum ' + numbers);
     return this.mathService.accumulate(numbers);
   }
+
+  @MessagePattern({ cmd: 'sum' })
+  sum2(numbers: number[]): number {
+    this.logger.log('sum2 ' + numbers);
+    return this.mathService.accumulate(numbers);
+  }
+
 }
