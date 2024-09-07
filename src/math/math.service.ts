021ad5cf861f0088af185c84c78714eb0b77fdea
Manita Intharachot <64160144@go.buu.ac.th>
2024-09-07 20:28:06 +0700

    create Post math and logger

--- src/math/math.service.ts
+++ src/math/math.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MathService {}
+export class MathService {
+  accumulate(data: number[]): number {
+    return (data || []).reduce((a, b) => a + b);
+  }
+}
